ea75f34eaca9f9d7acc042c3ef4433a950484f46
CPonty <christopher.ponticello@uqconnect.edu.au>
2014-06-06 12:46:50 +1000

    Added help text; decreased false-positive colour matching with an erode() on the histogram sample

--- test/cv-backproject.py
+++ test/cv-backproject.py
@@ -14,6 +14,7 @@ import cv2
 import sys
 import os
 import re
+import socket
 
 def purge_pyc():
     global dbg_file
@@ -24,12 +25,36 @@ def purge_pyc():
             os.remove(os.path.join(folder,f))
 
 purge_pyc()
-from cvcommon import *
+try:
+    from lib.cvcommon import *
+except ImportError, e:
+    print "Failed to start: Could not import lib/cvcommon.py"
+    exit(1)
+try:
+    from config import *
+except ImportError, e:
+    print "Failed to start: Could not import config.py"
+    exit(1)
+try:
+    UDP_ADDR
+    UDP_PORT
+    CV_WEBCAM
+except NameError as e:
+    print("Failed to start: config variable "+e.message)
+    exit(1)
 purge_pyc()
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(CV_WEBCAM)
+ret, _ = cap.read()
+if not ret:
+    print "Failed to start: Could not get frame from Webcam %d" % CV_WEBCAM
+    exit(1)
+
+print "Press 'h' for help"
 print "Press 'q' to quit"
 
+HELP_MODE=True
+UDP_SEND=False
 FPS_PRINT=False
 BLOB_TRACK=True
 HIST_RESAMPLE=True
@@ -43,7 +68,7 @@ HSV_H1=160
 HSV_S0=0
 HSV_S1=30
 HSV_S2=60
-HSV_V0=80
+HSV_V0=40
 K1SIZE=5
 K2SIZE=10
 TELEMETRY = {
@@ -51,13 +76,22 @@ TELEMETRY = {
     "y": None,
     "w": None
 }
+SRC_NAMES = [
+    "Calibration View",
+    "Colour Match View",
+    "Thresholding View",
+    "Object Match View",
+    "Object Tracking View",
+    "Object Tracking View"
+]
 threshVal=60
 
 t = [time()]
 fps=0
 f=0
-(xx,yy,ww,hh) = (640/2-60,480/2-80,120,160)
-src=5
+#(xx,yy,ww,hh) = (640/2-60,480/2-80,120,160)
+(xx,yy,ww,hh) = (0, 0, 0, 0)
+src=6
 grab=False
 im=None
 imArray=[None]*8
@@ -68,6 +102,18 @@ hist2=np.zeros((180,256),np.uint8)
 trackingHist=None
 drawBox=False
 pause=False
+
+def udp_send(msg):
+    global UDP_PORT, UDP_ADDR
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        l = sock.sendto(msg, (UDP_ADDR, UDP_PORT))
+    except Exception as e:
+        print "UDP send failed: "+e.message
+        exit(1)
+    finally:
+        if l != len(msg):
+            print "UDP send failed: send length (%d) != message length (%d)" % l, len(msg)
 
 def mouseclick(event,x,y,flags,param):
     global xx,yy,ww,hh,drawBox
@@ -82,52 +128,93 @@ def mouseclick(event,x,y,flags,param):
 
 def keyboard(waitFor):
     global pause, src, threshVal, grab, HIST_MIN_STRENGTH, HIST_ROI_DILATION, \
-        HIST_SAMP_DILATION
+        HIST_SAMP_DILATION, UDP_SEND, BLOB_TRACK, HELP_MODE
     k = (cv2.waitKey(waitFor) & 0xFF)
     if k == ord('q'):
         return True
-    elif ( ord('1')<=k<=ord('6')):
-        src=k-ord('0')
-        print 'source',src
-    elif k == ord('p'):
+    #elif ( ord('1')<=k<=ord('6')):
+    #    src=k-ord('0')
+    #    print 'source',src
+    elif k == ord('h'):
+        HELP_MODE = not HELP_MODE
+    elif k == ord('1'):
+        src = 1
+        print 'source',k
+    elif k == ord('2'):
+        src = 2
+        print 'source',k
+    elif k == ord('3'):
+        src = 4
+        print 'source',k
+    elif k == ord('4'):
+        src = 6
+        print 'source',k
+    elif k == ord(' ') or k == ord('p'):
         pause = not(pause)
-    elif k == ord('g'):
-        grab=True
-        if pause: 
-            src=2
-            print "done"
-        else: 
-            src=1
-            print 'sampling...',
-            sys.stdout.flush()
-        pause = not(pause)
-    elif k == ord('['):
-        HIST_ROI_DILATION = max(0,HIST_ROI_DILATION-1)
-        print "HIST_ROI_DILATION=", HIST_ROI_DILATION
-    elif k == ord(']'):
-        HIST_ROI_DILATION = min(255,HIST_ROI_DILATION+2)
-        print "HIST_ROI_DILATION=", HIST_ROI_DILATION
-    elif k == ord('-'):
-        HIST_SAMP_DILATION = max(0,HIST_SAMP_DILATION-1)
-        print "HIST_SAMP_DILATION=", HIST_SAMP_DILATION
-    elif k == ord('='):
-        HIST_SAMP_DILATION = min(255,HIST_SAMP_DILATION+2)
-        print "HIST_SAMP_DILATION=", HIST_SAMP_DILATION
+    elif k == ord('c'):
+        if src == 1:
+            grab = True
+        else:
+            src = 1
+        #grab=True
+        #if pause:
+        #    src=2
+        #    print "done"
+        #else:
+        #    src=1
+        #    print 'sampling...',
+        #    sys.stdout.flush()
+        #pause = not(pause)
+    elif k == ord('u'):
+        UDP_SEND = not UDP_SEND
+    elif k == ord('t'):
+        BLOB_TRACK = not BLOB_TRACK
+    #elif k == ord('['):
+    #    HIST_ROI_DILATION = max(0,HIST_ROI_DILATION-1)
+    #    print "HIST_ROI_DILATION=", HIST_ROI_DILATION
+    #elif k == ord(']'):
+    #    HIST_ROI_DILATION = min(255,HIST_ROI_DILATION+2)
+    #    print "HIST_ROI_DILATION=", HIST_ROI_DILATION
+    #elif k == ord('-'):
+    #    HIST_SAMP_DILATION = max(0,HIST_SAMP_DILATION-1)
+    #    print "HIST_SAMP_DILATION=", HIST_SAMP_DILATION
+    #elif k == ord('='):
+    #    HIST_SAMP_DILATION = min(255,HIST_SAMP_DILATION+2)
+    #    print "HIST_SAMP_DILATION=", HIST_SAMP_DILATION
     return False
 
 displayIm = np.zeros((480,640+256,3), np.uint8)
-blackTopBar = np.zeros((295,256),np.uint8)
+blackTopBar = np.zeros((303,256),np.uint8)
 blackBar = np.zeros((4,256),np.uint8)
 whiteBar = np.zeros((4,256),np.uint8)
 white1Bar = np.zeros((1,256),np.uint8)
 whiteBar.fill(255)
 white1Bar.fill(255)
+FONT_SIZE = 0.5
+
+def text_drawsize(s):
+    return cv2.getTextSize(s, cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, 2)
+
+def drawtext_leftalign(im,msg,x,y):
+    (tw,th), tbl = text_drawsize(msg)
+    cv2.rectangle(im, (x, y-th-tbl-2), (x+tw, y), cv2_black, -1)
+    cv2.putText(displayIm, msg, (x, y-tbl), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, (0,255,0))
+
+def drawtext_centered(im,msg,x,y):
+    (tw,th), tbl = text_drawsize(msg)
+    cv2.rectangle(im, (x-tw/2, y-th-tbl-2), (x+tw/2, y), cv2_black, -1)
+    cv2.putText(displayIm, msg, (x-tw/2, y-tbl), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, (0,255,0))
+
 def display():
-    global imArray,pause,xx,yy,ww,hh,src
+    global imArray,pause,xx,yy,ww,hh,src,HELP_MODE,pause,FONT_SIZE
     # Display the resulting frame
     imArray[1] = imArray[0].copy()
     cv2.rectangle(imArray[1],(xx,yy),(xx+ww,yy+hh),(0,255,0),2)
     if len(imArray[src].shape) > 2:
+        #if HELP_MODE:
+        #    imArrayColor = cv2.cvtColor(cv2.cvtColor(imArray[src], cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
+        #else:
+        #    imArrayColor = imArray[src]
         imArrayColor = imArray[src]
     else:
         imArrayColor = cv2.cvtColor(imArray[src], cv2.COLOR_GRAY2BGR)
@@ -139,9 +226,9 @@ def display():
         # 480 = 5*2 + 4*2 + 462/6 | 77
         histW = 27
         sidebar = np.vstack((blackTopBar,whiteBar,
-            blackBar,roiHist2[:histW,:],white1Bar,roiHist2[-histW:,:],
-            whiteBar,hist2[:histW,:],white1Bar,hist2[-histW:,:],
-            whiteBar,trackingHist[:histW,:],white1Bar,trackingHist[-histW:,:],
+            blackBar,roiHist2[-histW:,:], white1Bar, roiHist2[:histW,:],
+            hist2[-histW:,:], white1Bar, hist2[:histW,:],
+            trackingHist[-histW:,:], white1Bar, trackingHist[:histW,:],
             blackBar
         ))
         sidebar[sidebar > 0] = 255
@@ -150,6 +237,37 @@ def display():
         displayIm[:,640:] = sidebar
         #print sidebar.shape
     #cv2.imshow('im',displayIm)
+    if pause:
+        msg = "PAUSED"
+        drawtext_centered(displayIm, msg, 320, 480-25)
+        #(tw,th), tbl = text_drawsize(msg)
+        #cv2.rectangle(displayIm, (320-tw/2, 480-th-tbl-2), (320+tw/2, 480), cv2_black, -1)
+        #cv2.putText(displayIm, msg, (320-tw/2, 480-tbl), cv2.FONT_HERSHEY_SIMPLEX, FONT_SIZE, (0,255,0))
+    if HELP_MODE:
+        msgSet = ["CONTROLS:",
+            "",
+            "  1..4      Select View",
+            "  T         Tracking On/Off",
+            "  U         UDP On/Off",
+            "  C         Calibrate",
+            "  P/Space  Pause",
+            "  H         Help",
+            "  Q         Quit",
+            "",
+            "see config.py to configure UDP broadcast & webcam",
+            "",
+            "press 'h' to hide help text"#,
+        ]
+        for i, msg in enumerate(msgSet):
+            if msg != "":
+                drawtext_leftalign(displayIm, msg, 0, 60+22*i)
+
+    #TODO draw active screen
+    activeText = " ".join(['['*(src_i == src)+str(i+1)+']'*(src_i == src) for i,src_i in enumerate([1,2,4,6])])
+    activeText += "    " + SRC_NAMES[src-1]
+    drawtext_centered(displayIm, activeText, 320, 20)
+    #print activeText
+    displayIm[:,640,:] = 255
     cv2.imshow('PringleCV',displayIm)
 
 #----------------------------------------------------------------------
@@ -187,36 +305,47 @@ while(True):
     if imArray[src]!=None: display()
     if pause: 
 #        keyboard(100)
-        _,_ = cap.read()
+        ret, _ = cap.read()
+        if not ret:
+            print "Failed to start: Could not get frame from Webcam %d" % CV_WEBCAM
+            exit(1)
         continue
 
     # Grab RoI
-    if (grab==True and imArray[0]!=None and ww>2 and hh>2):
-        roi = im[yy:yy+hh, xx:xx+ww]
-        hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-        roiHist= cv2.calcHist([hsv],[0,1], None, [180,256], [0,180,0,256])
-        roiHist[HSV_H0:HSV_H1,:] = 0
-        roiHist[:,HSV_S0:HSV_S1] = 0
-        for i in xrange(HSV_S1,HSV_S2):
-            roiHist[:,i] *= float(i-HSV_S1)/(HSV_S2-HSV_S1)
-        roiHist[roiHist < HIST_MIN_STRENGTH] = 0
-        roiHist2= roiHist.copy()
-        print "max",np.max(roiHist2),"mean",np.mean(roiHist2)
-        cv2.normalize(roiHist2,roiHist2,0,255,cv2.NORM_MINMAX)
-        print "max",np.max(roiHist2),"mean",np.mean(roiHist2)
-        trackingHist = roiHist.copy()
-        #plot histogram. gotta go fast!
-        #if HIST_PLOT: 
-        #    plt.imshow(roiHist,interpolation = 'nearest')
-        #    plt.show()
-        #
-        grab=False
-        src=2
+    if grab==True and imArray[0]!=None:
+        if ww>2 and hh>2:
+            roi = im[yy:yy+hh, xx:xx+ww]
+            hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
+            roiHist= cv2.calcHist([hsv],[0,1], None, [180,256], [0,180,0,256])
+            roiHist[HSV_H0:HSV_H1,:] = 0
+            roiHist[:,HSV_S0:HSV_S1] = 0
+            for i in xrange(HSV_S1,HSV_S2):
+                roiHist[:,i] *= float(i-HSV_S1)/(HSV_S2-HSV_S1)
+            roiHist[roiHist < HIST_MIN_STRENGTH] = 0
+            roiHist2= roiHist.copy()
+            #print "max",np.max(roiHist2),"mean",np.mean(roiHist2)
+            cv2.normalize(roiHist2,roiHist2,0,255,cv2.NORM_MINMAX)
+            #print "max",np.max(roiHist2),"mean",np.mean(roiHist2)
+            trackingHist = roiHist.copy()
+            #plot histogram. gotta go fast!
+            #if HIST_PLOT:
+            #    plt.imshow(roiHist,interpolation = 'nearest')
+            #    plt.show()
+            #
+            grab=False
+            src=2
+            print "Calibration: took sample from (%d,%d) to (%d,%d)" % (xx,yy,xx+ww,yy+hh)
+        else:
+            print "Calibration: aborted, selection box too small"
         
 
     # Capture frame-by-frame
     ret, im = cap.read()
+    if not ret:
+        print "Webcam %d disconnected - exiting..." % CV_WEBCAM
+        exit(1)
     im = cv2.medianBlur(im, 5)
+    #im = cv2.blur(im, (7,7))
     im = cv2.resize(im, (640,480))
     imArray=[im.copy() for i in xrange(len(imArray))]
 
@@ -258,10 +387,10 @@ while(True):
         imArray[2] = dst #grayscale distance
         ret,thresh = cv2.threshold(dst,threshVal,255,0)
         imArray[3] = thresh
-        val = hsvt[:,:,2]
-        val[val < HSV_V0] = 0
-        val[val > 0] = 255
-        thresh = cv2.bitwise_and(thresh, val)
+        #val = hsvt[:,:,2]
+        #val[val < HSV_V0] = 0
+        #val[val > 0] = 255
+        #thresh = cv2.bitwise_and(thresh, val)
         thresh3 = cv2.merge((thresh,thresh,thresh)) #threshold, 3-channel
         img4 = cv2.bitwise_and(im,thresh3) #combined image
         imArray[4] = img4
@@ -270,12 +399,23 @@ while(True):
         #test: blob tracking; redo the histogram
         # BLOB_TRACK #
         if BLOB_TRACK==False: continue
-        thresh = cv2.dilate(thresh, None)
+        #thresh = cv2.dilate(thresh, None)
+        #thresh = cv2.erode(thresh, None)
+        #thresh = cv2.dilate(thresh, None)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(K1SIZE,K1SIZE))
         kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(K2SIZE,K2SIZE))
         cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,thresh)
         cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel,thresh)
         cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel2,thresh)
+        #thresh = cv2.dilate(thresh, None)
+        val = hsvt[:, :, 2]
+        val[val < HSV_V0] = 0
+        val[val > HSV_V0] = 255
+        thresh = cv2.bitwise_and(thresh, val)
+        thresh = cv2.dilate(thresh, None)
+        thresh = cv2.erode(thresh, None)
+        #thresh = cv2.erode(thresh, None)
+        # # # #
         thresh2 = thresh.copy()
         contours,hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
         best_cnt = None
@@ -306,6 +446,7 @@ while(True):
         #cv2.drawContours(img4,cntCandidates,-1,(255,255,255),cv2.cv.CV_FILLED)
         cv2.drawContours(img4,cntCandidates,-1,(255,0,255),thickness=1)
         cv2.drawContours(imArray[5],cntCandidates,-1,(255,0,255),thickness=1)
+        cv2.drawContours(imArray[6],cntCandidates,-1,(255,0,255),thickness=1)
         #print len(cntCandidates)
         if (best_cnt != None):
             x,y,w,h = cv2.boundingRect(best_cnt)
@@ -320,9 +461,9 @@ while(True):
             #cv2_minRectW(minRect)
             cv2.drawContours(img4,[minRect],0,(0,255,255),1)
             cv2.drawContours(imArray[5],[minRect],0,(0,255,255),1)
-            cv2.drawContours(imArray[6],[minRect],0,(0,255,255),2)
+            cv2.drawContours(imArray[6],[minRect],0,(0,255,255),1)
             (canw,canh),wl,hl = cv2_minRectAxes(minRect)
-            cv2.line(imArray[6],wl[0],wl[1],cv2_yellow,2)
+            cv2.line(imArray[6],wl[0],wl[1],cv2_yellow,1)
             hull = cv2.convexHull(best_cnt)
             cv2.drawContours(img4,[hull],0,(255,255,0),1)
             cv2.drawContours(imArray[5],[hull],0,(255,255,0),1)
@@ -344,6 +485,8 @@ while(True):
             hsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
             mask = np.zeros((hsv.shape[0],hsv.shape[1],1),np.uint8)
             cv2.drawContours(mask,[best_cnt],0,255,-1)
+            #mask = cv2.dilate(mask, None)
+            # # # #
             #cv2.drawContours(mask,[hull],0,255,-1)
 #TODO - erode sample mask & delete small blobs again
             imArray[7] = mask.copy()
@@ -354,6 +497,7 @@ while(True):
             hist2[HSV_H0:HSV_H1,:] = 0
             hist2[:,HSV_S0:HSV_S1] = 0
             cv2.normalize(hist2,hist2,0,255,cv2.NORM_MINMAX)
+            hist2 = cv2.erode(hist2, None)
 #TODO - apply distance (roiHist, hist2) to hist2
             #gradient-scale the saturation weighting in the resampled hist
             for i in xrange(HSV_S1,HSV_S2):
